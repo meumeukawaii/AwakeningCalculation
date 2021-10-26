a3410d87c0752a9219b41b0855195c38d6e699a0
meumeukawaii <75741504+meumeukawaii@users.noreply.github.com>
2021-10-26 14:01:47 +0900

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -111,6 +111,7 @@ function calculate(){
         
         for(i = level[0].value; i < Level.length; i++){ //한번 레벨 올라갔으므로 +1 된 상태임
             if(exp_all < Level[i][1]){
+                nextExp = exp_all
                 break;
             }
             else{
